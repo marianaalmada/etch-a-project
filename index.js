460b17c51ca8d7a221d62a3bcd56df3eb8f4f03a
unknown <marianaalmada15@outlook.com>
2022-12-17 21:35:35 -0300

    grid creado y estilos agregados

---
+++ index.js
@@ -0,0 +1,5 @@
+for (let i = 0; i < 256; i++) {
+    const container = document.querySelector('.grid-container');
+    const square = document.createElement('div');
+    container.appendChild(square);
+}
