8e9e9953c2cd1a61c93978daaac0ac4c993639b3
unknown <marianaalmada15@outlook.com>
2022-12-20 00:49:00 -0300

    funcionalidad que cambia de color al pasar por cada cuadrado agregada

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
+const container = document.querySelector('.grid-container');
+
 for (let i = 0; i < 256; i++) {
-    const container = document.querySelector('.grid-container');
     const square = document.createElement('div');
+    // square.classList.add('square');
     container.appendChild(square);
 }
+
+function paint(event) {
+    const targetElement = event.target;
+    targetElement.style.backgroundColor = 'black';
+}
+
+container.addEventListener('mousedown', (e) => {
+    paint(e);
+
+    container.addEventListener('mousemove', paint);
+
+    window.addEventListener('mouseup', () => {
+        console.log('stop');
+        container.removeEventListener('mousemove', paint);
+    });
+});
