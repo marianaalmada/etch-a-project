892242f8add8baf5bafec8d3bee58602605a51d6
unknown <marianaalmada15@outlook.com>
2022-12-22 12:45:26 -0300

    boton que cambia el tamaño del grid creado y cambios en estilos

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const container = document.querySelector('.grid-container');
+const button = document.querySelector('#button');
 
-for (let i = 0; i < 256; i++) {
-    const square = document.createElement('div');
-    // square.classList.add('square');
-    container.appendChild(square);
+createGrid(16);
+
+function createGrid(size) {
+    container.style.cssText = `grid-template-columns: repeat(${size}, 1fr); 
+            grid-template-rows: repeat(${size}, 1fr);`;
+    const number = size * size;
+
+    for (let i = 0; i < number; i++) {
+        const square = document.createElement('div');
+        container.appendChild(square);
+        square.classList.add('square');
+    }
 }
 
 function paint(event) {
@@ -11,13 +20,31 @@ function paint(event) {
     targetElement.style.backgroundColor = 'black';
 }
 
+function resizeGrid(input) {
+    const container = document.querySelector('.grid-container');
+    const square = document.querySelectorAll('.square');
+    
+    if(!isNaN(input) && input >= 16 && input <= 64) {
+        console.log('Esta dentro del rango');
+        for (const i of square) {
+            container.removeChild(i);
+        }
+        createGrid(input);
+    }
+}
+
+button.addEventListener('click', () => {
+    const input = prompt('Input the number of squares per size');
+    const numbericValue = Number(input);
+    resizeGrid(numbericValue);
+});
+
 container.addEventListener('mousedown', (e) => {
     paint(e);
 
     container.addEventListener('mousemove', paint);
 
     window.addEventListener('mouseup', () => {
-        console.log('stop');
         container.removeEventListener('mousemove', paint);
     });
 });
